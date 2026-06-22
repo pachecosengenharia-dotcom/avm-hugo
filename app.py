1d21817ead54e539e66d1af1e3b31edd103a62cc
pachecosengenharia-dotcom <pachecosengenharia@gmail.com>
2026-06-22 02:14:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 
-# Esta função resolve o problema da "interrogação" removendo acentos
+import unicodedata
+
 def limpar(t):
+    # NFKD decompõe 'ç' em 'c' + 'cedilha'
+    # Combina com o filtro que remove qualquer caractere que não seja uma letra base
     return "".join([c for c in unicodedata.normalize('NFKD', str(t)) if not unicodedata.combining(c)])
+
+
 
 def gerar_laudo_final(res, info, features, inputs, fig):
     buf = io.BytesIO()
