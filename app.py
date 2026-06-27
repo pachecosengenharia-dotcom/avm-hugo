7197fd284825e5dcf82365d1e171d3a36d51a556
pachecosengenharia-dotcom <pachecosengenharia@gmail.com>
2026-06-27 18:18:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
+import textwrap
 import unicodedata
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
